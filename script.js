b18263d05bbaa392699311f4926d6e383ae948b7
Márcio Diniz <marcio.sccp10@gmail.com>
2023-05-08 19:17:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,3 @@ function aoInicio() {
 
 // Seleciona elementos
 const principal = document.querySelector('.principal');
-
-// Cria o container de produtos na tela inicial
-const ofertasIniciais = document.createElement('div');
-ofertasIniciais.classList.add('ofertas-iniciais');
-principal.appendChild(ofertasIniciais);
-
-// Cria os produtos na tela inicial
-const produto1 = document.createElement('div');
-produto1.classList.add('produto1');
-produto1.innerHTML = 'Produto 01';
-ofertasIniciais.appendChild(produto1);
-
-const produto2 = document.createElement('div');
-produto2.classList.add('produto2');
-produto2.innerHTML = 'Produto 02';
-ofertasIniciais.appendChild(produto2);
-
-const produto3 = document.createElement('div');
-produto3.classList.add('produto3');
-produto3.innerHTML = 'Produto 03';
-ofertasIniciais.appendChild(produto3);
