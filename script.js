0ed23989363f203ed3b11cb2c5de4f6a1de93e50
Márcio Diniz <marcio.sccp10@gmail.com>
2023-05-08 19:10:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Leva da página index à home imediatamente
 function aoInicio() {
     window.location = "pages/home.html";
 }
+
+// Seleciona elementos
+const principal = document.querySelector('principal');
+
+// Cria o container de produtos na tela inicial
+const ofertasIniciais = document.createElement('div');
+ofertasIniciais.classList.add('ofertas-iniciais');
+principal.appendChild(ofertasIniciais);
+
+// Cria os produtos na tela inicial
